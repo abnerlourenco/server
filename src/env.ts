2d06415428aa37e3b1526134c77b6c77a6fc13d9
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2025-03-01 12:11:52 -0300

    feat: created redis and pg db connections. Added drizzle ORM. created first table and migration to subscriptions and function to subscribe to event

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   BASEURL_WEB_ORIGIN: z.string().default(''),
+  POSTGRES_URL: z.string().url(),
+  REDIS_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
