998bafd61a35c9fd68edc90e42b4bc4f93750822
Abner da Rocha Lourenço <abner2010_hm@hotmail.com>
2025-03-01 15:06:51 -0300

    feat: added access invite link route and count link access number

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { env } from './env'
+import { accesInviteLinkRoute } from './routes/access-invite-link.routes'
 import { subscribeToEventRoute } from './routes/subscribe-to-event.routes'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -35,6 +36,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(subscribeToEventRoute)
+app.register(accesInviteLinkRoute)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server is running!')
